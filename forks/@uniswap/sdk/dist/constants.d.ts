6db1fb16b3033979f1dc466847725891fc02aa64
Harry Wang <0xharrywang@gmail.com>
2024-12-14 11:58:01 +0800

    fix forks

--- forks/@uniswap/sdk/dist/constants.d.ts
+++ forks/@uniswap/sdk/dist/constants.d.ts
@@ -5,7 +5,9 @@ export declare enum ChainId {
     ROPSTEN = 3,
     RINKEBY = 4,
     GÖRLI = 5,
-    KOVAN = 42
+    KOVAN = 42,
+    // add
+    SEPOLIA = 11155111
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
